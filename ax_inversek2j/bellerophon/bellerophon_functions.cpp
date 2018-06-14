74d2892ad435648c29db96ca26027d091818e3c9
nnkhoa <n.nhukhoa2209@gmail.com>
2018-06-14 11:11:23 +0200

    updated on inversej2k, fix bellerophon functions, taken care of NaNs value from ouput file

--- ax_inversek2j/bellerophon/bellerophon_functions.cpp
+++ ax_inversek2j/bellerophon/bellerophon_functions.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<fstream>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "vpa_n.h"
 bool vpa_n::VPA::UPCASTING = false;
@@ -8,6 +10,15 @@ bool vpa_n::VPA::UPCASTING = false;
 
 
 #include "inversek2j.hpp" // function definition
+
+double toDouble(::std::string numberString){
+	int len = numberString.size();
+	char* charArray = new char [len+1];
+	memset(charArray, 0, len + 1);
+	strcpy (charArray, numberString.c_str());
+
+	return atof(charArray);
+}
 
 extern "C" double BELLERO_getError() {    
     const char* inputPath = "/opt/ax_inversek2j/test.data/input/theta_1000K.data";
@@ -27,31 +38,54 @@ extern "C" double BELLERO_getError() {
         exit ( 1 );
 
     }
+	int count = 0;
 
-
-	double WCE = 0.0;
-	double tmp;
-
-	double greal, gima;
-	double axreal, axima;
-	double diffreal, diffimg;
+	double absError = 0.0;
+	double nominator, denominator, error;
 	
+	::std::string oValue0, oValue1;
+	::std::string axValue0, axValue1;
+	double diff0, diff1;
+	
+	::std::cout << "Calculating Error: \n";	
 	while (!oracle.eof () && !axc.eof()) {
-	
-		oracle >> greal >> gima;
-		axc >> axreal >> axima;
-		diffreal =  (greal - axreal)  ;
-		diffimg = (gima - axima);
+		//::std::cout << count << "\n";	
+		oracle >> oValue0 >> oValue1;
+		axc >> axValue0 >> axValue1;
+		
+		//::std::cout << oValue0 << " " << oValue1 << "\n";
+		
+		//check if any value is NaN
+		if (oValue0.compare("nan") == 0 || oValue1.compare("nan") == 0 || axValue0.compare("nan") == 0 || axValue1.compare("nan") == 0) {
+			::std::cout << "NaN\n";
+			count ++;
+			absError += 1;
+			continue;
+		} 
+		
+		//calculate abs error
+		diff0 =  (toDouble(oValue0) - toDouble(axValue0))  ;
+		diff1 = (toDouble(oValue1) - toDouble(axValue1));
 
-
-		tmp = sqrt (  (diffreal*diffreal) + (diffimg*diffimg)  );
-		//tmp = abs (good_v - axc_v);
-		if (tmp > WCE) {
-			WCE = tmp;
+		nominator = sqrt(pow(diff0, 2) + pow(diff1, 2));
+		denominator = sqrt(pow(toDouble(oValue0), 2) + pow(toDouble(oValue1), 2));
+		
+		if (denominator == 0) {
+			error = 1.0;
+		}else if (nominator > denominator) {
+			error = 1.0;
+		}else {
+			error = nominator/denominator;
 		}
 
+		absError += error;
+		count++;
+
 	}
-	return WCE;
+	::std::cout << "Done calculating\n";
+	::std::cout << absError << " " << count <<"\n";
+	//return relative average error
+	return (double) absError/count;
 
 
 }
